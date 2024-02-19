5f48a4aeb0676660478ee26993c5ece5eb1abc57
Adya Sena Pamungkas <adyasena01@mail.ugm.ac.id>
2024-02-20 01:35:07 +0700

    feat: update hero section

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,8 +1,5 @@
 import React, { useEffect, useState } from "react";
-import BgHome from "../assets/home.jpg";
-import LogoKominfo from "../assets/logo_kominfo.png";
-import LogoPurworejo from "../assets/logo_purworejo.png";
-import Hug from "../assets/hug.mp3";
+import BgHome from "../assets/bg.webp";
 
 const Home = () => {
   const scrollToBottom = () => {
@@ -11,32 +8,17 @@ const Home = () => {
       behavior: 'smooth',
     });
   };
-  function start() {
-    new Audio(Hug).play()
-  }
 
   return (
     <div className="w-full h-screen bg-cover bg-center bg-no-repeat" style={{backgroundImage: `url(${BgHome})`}}>
       <div className="w-full h-screen bg-black bg-opacity-60">
-        <div className="flex flex-col gap-10 h-full container mx-auto items-center font-poppins text-white">
-          <div className="flex flex-row justify-center items-center mt-32 h-24">
-            <img src={LogoKominfo} alt="logo kominfo" className="h-full w-auto scale-95" />
-            <box className="h-full mx-6 p-[0.07rem] rounded-sm bg-white"></box>
-            <img src={LogoPurworejo} alt="lambang purworejo" className="h-full w-auto" />
+        <div className="flex flex-col h-full gap-2 container mx-auto font-poppins text-white justify-center items-center">
+          <div className="font-medium text-xl text-left ml-[-33rem]">
+            ADYA SENA PAMUNGKAS
           </div>
-          <div className="font-semibold text-3xl text-center leading-normal">
-            Agenda Penggunaan Fasilitas VCON<br />
-            Dinas Komunikasi, Informatika, Statistik dan Persandian<br />
-            Kabupaten Purworejo
-          </div>
-          <div className="mt-10">
-            <button className="bg-blue-light text-white font-normal py-2 px-6 rounded-full transform duration-300 ease hover:bg-white hover:text-black" 
-              onClick={start}>
-                Pencet Ini Tan
-            </button>
-          </div>
-          <div>
-          
+          <div className="font-semibold text-9xl text-left">
+            FRONTEND<br />
+            DEVELOPER
           </div>
         </div>
       </div>
