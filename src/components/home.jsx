4aadc56df102ba1a42e9b86f060e136c5cee8502
Adya Sena Pamungkas <adyasena01@mail.ugm.ac.id>
2024-02-20 09:40:39 +0700

    feat: update home content

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -11,15 +11,13 @@ const Home = () => {
 
   return (
     <div className="w-full h-screen bg-cover bg-center bg-no-repeat" style={{backgroundImage: `url(${BgHome})`}}>
-      <div className="w-full h-screen bg-black bg-opacity-60">
-        <div className="flex flex-col h-full gap-2 container mx-auto font-poppins text-white justify-center items-center">
-          <div className="font-medium text-xl text-left ml-[-33rem]">
-            ADYA SENA PAMUNGKAS
-          </div>
-          <div className="font-semibold text-9xl text-left">
-            FRONTEND<br />
-            DEVELOPER
-          </div>
+      <div className="flex flex-col h-full gap-2 container mx-auto font-inter text-white justify-center items-center">
+        <div className="font-medium text-xl text-left ml-[-33rem]">
+          ADYA SENA PAMUNGKAS
+        </div>
+        <div className="font-semibold text-9xl text-left">
+          FRONTEND<br />
+          DEVELOPER.
         </div>
       </div>
     </div>
