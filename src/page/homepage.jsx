882d4c50d2ae2e0f0da559c54bb6523bf2578688
Adya Sena Pamungkas <adyasena01@mail.ugm.ac.id>
2024-02-20 09:41:44 +0700

    chore: remove unnecessary files

--- src/page/homepage.jsx
+++ src/page/homepage.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Navbar from "../components/navbar";
 import Home from "../components/home";
-import Agenda from "../components/agenda";
 
 const Homepage = () => {
   return (
     <>
       <Navbar />
       <Home />
-      <Agenda />
     </>
   )
 }
