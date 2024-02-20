e171a0f74351e93209e3f78633cc5854eefa5b9e
Adya Sena Pamungkas <adyasena01@mail.ugm.ac.id>
2024-02-20 09:40:19 +0700

    feat: update navbar content

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import LogoKominfo from "../assets/logo_kominfo.png";
+import Logo from "../assets/logo.png";
 
 const Navbar = () => {
   const [scroll, setScroll] = useState(false);
@@ -16,18 +16,32 @@ const Navbar = () => {
 
   return (
     <div className="navbar">
-      <div className={"navbar bg-gradient-to-l from-blue-light to-blue-primary " + (scroll ? "opacity-100" : "h-24 opacity-0")}>
+      <div className={"navbar bg-black " + (scroll ? "opacity-100" : "h-24 opacity-0")}>
       </div>
       <div className={"z-[1] font-poppins sticky flex flex-row container mx-auto px-12 text-center items-center text-lg font-semibold " +
         "justify-between transform duration-300 ease overflow-hidden " + (scroll ? "h-16" : "h-24")}>
-        <div className="flex flex-row justify-start items-center text-white h-full">
-          <img src={LogoKominfo} alt="logo kominfo" className={"m-2 transform duration-300 ease " + (scroll ? "w-8" : "w-0 mr-[-4px]")}/>
-          <p className="transform">Agenda VCON</p> 
-        </div>
-          <button className={"text-base border-2 font-normal py-2 px-6 rounded-full transform duration-300 ease "
-            + (scroll ? "bg-white text-blue-light border-white hover:bg-blue-light hover:text-white" : "text-white border-blue-light hover:bg-blue-light")}>
-            Login Admin
-          </button> 
+          <div className="flex flex-row justify-start items-center text-white h-full">
+            <img src={Logo} alt="logo kominfo" className={"m-2 transform duration-300 ease " + (scroll ? "w-8" : "w-0 mr-[-4px]")}/>
+            <p className="transform">Adya Sena</p> 
+          </div>
+          <div className="flex gap-6">
+            <button className="text-base py-2 px-6 rounded-full transform duration-300 ease text-white hover:text-green">
+                Home
+            </button>
+            <button className="text-base py-2 px-6 rounded-full transform duration-300 ease text-white hover:text-red">
+                About
+            </button>
+            <button className="text-base py-2 px-6 rounded-full transform duration-300 ease text-white hover:text-blue">
+                Skills
+            </button>
+            <button className="text-base py-2 px-6 rounded-full transform duration-300 ease text-white hover:text-pink">
+                Works
+            </button>
+            <button className={"text-base border-2 font-normal py-2 px-6 rounded-full transform duration-300 ease "
+              + (scroll ? "bg-white text-black border-white hover:bg-white hover:text-black" : "text-white border-white hover:bg-white hover:text-black")}>
+                Contact
+            </button>
+          </div>
       </div>
     </div>
   )
