9fb5ce8efa38a19a797432f8f1b0d9288d3e2991
Adya Sena Pamungkas <adyasena01@mail.ugm.ac.id>
2024-02-21 22:56:09 +0700

    feat: add about section

--- src/page/homepage.jsx
+++ src/page/homepage.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import Navbar from "../components/navbar";
 import Home from "../components/home";
+import About from "../components/about";
 
 const Homepage = () => {
   return (
     <>
       <Navbar />
       <Home />
+      <About />
     </>
-  )
-}
+  );
+};
 
-export default Homepage ;
+export default Homepage;
